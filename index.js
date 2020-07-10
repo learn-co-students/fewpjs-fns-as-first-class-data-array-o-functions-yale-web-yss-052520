4c6eb6cb8a375a6c12668b4a391309416f3f0dcf
zayzay500 <lindsay.pierce@yale.edu>
2020-07-10 10:19:56 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,45 @@
+const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
+
+function exerciseDog(dogName, dogBreed) {
+    const results = []
+    routine.forEach(task => {
+        results.push(task(dogName, dogBreed))
+    })
+    return results
+}
+
+function wakeDog(dogName, dogBreed) {
+    const string = `Wake ${dogName} the ${dogBreed}`
+    console.log(string)
+    return string
+}
+
+function leashDog(dogName, dogBreed) {
+    const string = `Leash ${dogName} the ${dogBreed}`
+    console.log(string)
+    return string
+}
+
+function walkToPark(dogName, dogBreed) {
+    const string = `Walk to the park with ${dogName} the ${dogBreed}`
+    console.log(string)
+    return string
+}
+
+function throwFrisbee(dogName, dogBreed) {
+    const string = `Throw the frisbee for ${dogName} the ${dogBreed}`
+    console.log(string)
+    return string
+}
+
+function walkHome(dogName, dogBreed) {
+    const string = `Walk home with ${dogName} the ${dogBreed}`
+    console.log(string)
+    return string
+}
+
+function unleashDog(dogName, dogBreed) {
+    const string = `Unleash ${dogName} the ${dogBreed}`
+    console.log(string)
+    return string
+}
